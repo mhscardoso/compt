e830234336d9e252dd113ea7b76a584860099a0d
Matheus Cardoso <mhcardoso@poli.ufrj.br>
2026-04-14 22:03:37 -0300

    trabalho 5 | nota 100.0

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <math.h>
+#include <string>
 #include <vector>
+#include <map>
 
 // Define folder and mode
-#define WORK  004
+#define WORK  005
 #define JUDGE 0
 
+double NOTA = 0;
 
 #define STRINGFY(x) #x
 #define EXPAND(x)   STRINGFY(x)
@@ -17,11 +21,40 @@
 #include EXPAND(WORK/test.h)
 
 
+void execute_atomic_test(
+    const TestCase& t, 
+    const std::unordered_map<std::string, std::string>& esperados
+) {
+    string output = capture_output(t.func);
+
+    string esperado;
+    
+    try {
+        esperado = esperados.at(t.nome);
+    } catch (...) {
+        cout << t.nome << ": SEM RESULTADO ESPERADO\n";
+        return;
+    }
+
+    trim_final(esperado);
+    trim_final(output);
+
+    if (output == esperado) {
+        cout << t.nome << ": OK\n";
+        NOTA++;
+    } else {
+        cout << t.nome << ": FAIL\n";
+        cout << "Esperado:\n" << esperado << endl;
+        cout << "Obtido:\n" << output << endl;
+    }
+
+    divide();
+}
+
+
+
 int main(void) {
 
-#if JUDGE == 0
-
-    double NOTA = 0;
 	std::ios::sync_with_stdio(true);
 
 	divide();
@@ -29,44 +62,26 @@ int main(void) {
 
     auto esperados = ler_resultados(TEST_FILE);
 
+#if JUDGE == 0
+
     for (auto& t : testes) {
-        string output = capture_output(t.func);
-
-        string esperado;
-        
-        try {
-            esperado = esperados.at(t.nome);
-        } catch (...) {
-            cout << t.nome << ": SEM RESULTADO ESPERADO\n";
-            continue;
-        }
-
-        trim_final(esperado);
-        trim_final(output);
-
-        if (output == esperado) {
-            cout << t.nome << ": OK\n";
-            NOTA++;
-        } else {
-            cout << t.nome << ": FAIL\n";
-            cout << "Esperado:\n" << esperado << endl;
-            cout << "Obtido:\n" << output << endl;
-        }
-
-        divide();
+        execute_atomic_test(t, esperados);
     }
 
     divide();
     double nota = (NOTA / testes.size()) * 100;
 
-    cout << "Nota Final: " << setprecision(4) << nota << endl;
+    cout << "Nota Final: " 
+         << setprecision(4) << nota 
+         << endl;
 
     divide();
 
 #else
 
-    testes.at(JUDGE-1).func();
+    auto t = testes.at(JUDGE-1);
+    execute_atomic_test(t, esperados);
 
-#endif // JUDGE == ?
+#endif // JUDGE == 0
 
 }
